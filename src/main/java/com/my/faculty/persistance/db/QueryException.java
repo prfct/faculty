af24ec71462759165d300b2cd8cc79075f8f29f1
stein <petrohalko88@gmail.com>
2017-08-18 02:30:36 +0300

    commit before global changes

---
+++ src/main/java/com/my/faculty/persistance/db/QueryException.java
@@ -0,0 +1,10 @@
+package com.my.faculty.persistance.db;
+
+/**
+ * @author Oleksii Petrokhalko.
+ */
+public class QueryException extends RuntimeException {
+    public QueryException(Throwable cause) {
+        super(cause);
+    }
+}
