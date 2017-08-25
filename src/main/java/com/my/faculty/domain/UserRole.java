b02a50ed3c733725430db7fcd80ccfc860b345d0
stein <petrohalko88@gmail.com>
2017-08-25 15:29:21 +0300

    some changes

--- src/main/java/com/my/faculty/domain/UserRole.java
+++ src/main/java/com/my/faculty/domain/UserRole.java
@@ -4,7 +4,7 @@ package com.my.faculty.domain;
  * @author Oleksii Petrokhalko.
  */
 public enum UserRole {
-    ADMIN, DEFAULT;
+    ADMIN, DEFAULT, STUDENT_MANAGER, TEACHER_MANAGER;
 
     public static UserRole fromString(String string) {
         UserRole result = null;
